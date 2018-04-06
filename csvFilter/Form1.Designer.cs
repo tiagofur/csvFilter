d460163c96fd4cdcc346d2ef63e4aca8fbab0309
tiagofur <tiagofur@gmail.com>
2018-04-06 16:34:03 -0500

    First Versiom
    Primeira versao publicada do sistema de filtro de CSV

---
+++ csvFilter/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace csvFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.textBoxFileCSV = new System.Windows.Forms.TextBox();
+            this.dataGridViewDadosCSV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTeste = new System.Windows.Forms.Label();
+            this.comboBoxSeparador = new System.Windows.Forms.ComboBox();
+            this.comboBoxColum = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDadosCSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(819, 9);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Exportar";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // textBoxFileCSV
+            // 
+            this.textBoxFileCSV.Location = new System.Drawing.Point(62, 12);
+            this.textBoxFileCSV.Name = "textBoxFileCSV";
+            this.textBoxFileCSV.Size = new System.Drawing.Size(426, 20);
+            this.textBoxFileCSV.TabIndex = 1;
+            // 
+            // dataGridViewDadosCSV
+            // 
+            this.dataGridViewDadosCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDadosCSV.Location = new System.Drawing.Point(13, 38);
+            this.dataGridViewDadosCSV.Name = "dataGridViewDadosCSV";
+            this.dataGridViewDadosCSV.Size = new System.Drawing.Size(881, 371);
+            this.dataGridViewDadosCSV.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Archivo:";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(494, 10);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(28, 23);
+            this.btnSelectFile.TabIndex = 4;
+            this.btnSelectFile.Text = "...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Columna:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Separador:";
+            // 
+            // labelTeste
+            // 
+            this.labelTeste.AutoSize = true;
+            this.labelTeste.Location = new System.Drawing.Point(13, 416);
+            this.labelTeste.Name = "labelTeste";
+            this.labelTeste.Size = new System.Drawing.Size(56, 13);
+            this.labelTeste.TabIndex = 9;
+            this.labelTeste.Text = "labelTeste";
+            // 
+            // comboBoxSeparador
+            // 
+            this.comboBoxSeparador.FormattingEnabled = true;
+            this.comboBoxSeparador.Location = new System.Drawing.Point(593, 11);
+            this.comboBoxSeparador.Name = "comboBoxSeparador";
+            this.comboBoxSeparador.Size = new System.Drawing.Size(43, 21);
+            this.comboBoxSeparador.TabIndex = 10;
+            this.comboBoxSeparador.SelectedIndexChanged += new System.EventHandler(this.comboBoxSeparador_SelectedIndexChanged);
+            // 
+            // comboBoxColum
+            // 
+            this.comboBoxColum.FormattingEnabled = true;
+            this.comboBoxColum.Location = new System.Drawing.Point(692, 10);
+            this.comboBoxColum.Name = "comboBoxColum";
+            this.comboBoxColum.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxColum.TabIndex = 11;
+            this.comboBoxColum.SelectedIndexChanged += new System.EventHandler(this.comboBoxColum_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 454);
+            this.Controls.Add(this.comboBoxColum);
+            this.Controls.Add(this.comboBoxSeparador);
+            this.Controls.Add(this.labelTeste);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewDadosCSV);
+            this.Controls.Add(this.textBoxFileCSV);
+            this.Controls.Add(this.btnExport);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "CSV Filter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDadosCSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox textBoxFileCSV;
+        private System.Windows.Forms.DataGridView dataGridViewDadosCSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTeste;
+        private System.Windows.Forms.ComboBox comboBoxSeparador;
+        private System.Windows.Forms.ComboBox comboBoxColum;
+    }
+}
+
---
+++ csvFilter/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using ADODB;
+
+namespace csvFilter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            populateSeparator();
+        }
+
+        string[] filterCSV;
+        string[] filterExport;
+        string[] columnFilter;
+
+        string separator;
+        int filterColumm;
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+
+            fdlg.Title = "Select file";
+
+            fdlg.InitialDirectory = @"c:\";
+
+            fdlg.FileName = textBoxFileCSV.Text;
+
+            fdlg.Filter = "Text and CSV Files(*.txt, *.csv)|*.txt;*.csv|Text Files(*.txt)|*.txt|CSV Files(*.csv)|*.csv|All Files(*.*)|*.*";
+
+            fdlg.FilterIndex = 1;
+
+            fdlg.RestoreDirectory = true;
+
+            if (fdlg.ShowDialog() == DialogResult.OK)
+
+            {
+
+                textBoxFileCSV.Text = fdlg.FileName;
+
+                Import();
+
+                Application.DoEvents();
+
+                populateColumnFilter();
+            }
+        }
+
+        public static DataTable GetDataTable(string strFileName)
+        {
+            Connection oConn = new Connection();
+            oConn.Open("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + Path.GetDirectoryName(strFileName) + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\";", "", "", 0);
+            string strQuery = "SELECT * FROM [" + Path.GetFileName(strFileName) + "]";
+            Recordset rs = new Recordset();
+            System.Data.OleDb.OleDbDataAdapter adapter = new System.Data.OleDb.OleDbDataAdapter();
+            DataTable dt = new DataTable();
+            rs.Open(strQuery, "Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + Path.GetDirectoryName(strFileName) + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\";",
+            CursorTypeEnum.adOpenForwardOnly, LockTypeEnum.adLockReadOnly, 1);
+            adapter.Fill(dt, rs);
+            return dt;
+        }
+
+        private void Import()
+        {
+            if (textBoxFileCSV.Text.Trim() != string.Empty)
+            {
+                try
+                {
+                    DataTable dt = GetDataTable(textBoxFileCSV.Text);
+                    dataGridViewDadosCSV.DataSource = dt.DefaultView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void populateColumnFilter()
+        {
+            columnFilter = new string[dataGridViewDadosCSV.Columns.Count];
+
+            for (int columns = 0; columns < dataGridViewDadosCSV.Columns.Count; columns++)
+            {
+                columnFilter[columns] = dataGridViewDadosCSV.Columns[columns].HeaderText;
+            }
+
+            comboBoxColum.DataSource = columnFilter;
+        }
+
+        private void populateSeparator()
+        {
+            String[] myArray = { ";", "," };
+            comboBoxSeparador.DataSource = myArray;
+            //comboBoxSeparador.SelectedValueChanged += new EventHandler(this.comboBoxSeparador_SelectedValueChanged);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            createFilterExport();
+
+            for (int i = 0; i < filterCSV.Length-1; i++)
+            {
+                guardarCSVFiltrado(filterCSV[i]);
+            }
+
+            messageFinish();
+        }
+
+        private void createFilterExport()
+        {
+            filterExport = new string[dataGridViewDadosCSV.Rows.Count];
+
+            for (int rows = 0; rows < dataGridViewDadosCSV.Rows.Count - 1; rows++)
+            {
+                filterExport[rows] = dataGridViewDadosCSV[filterColumm, rows].Value.ToString();
+            }
+
+            HashSet<string> set = new HashSet<string>(filterExport);
+            filterCSV = new string[set.Count];
+            set.CopyTo(filterCSV);
+        }
+        
+        private void guardarCSVFiltrado(string filter)
+        {
+            string archivo = textBoxFileCSV.Text.Substring(0, textBoxFileCSV.Text.Length - 4);
+            string fileExportName = archivo + "_" + filter + ".csv";
+
+            // create one file gridview.csv in writing mode using streamwriter
+            StreamWriter sw = new StreamWriter(fileExportName);
+            // now add the gridview header in csv file suffix with "," delimeter except last one
+            for (int i = 0; i < dataGridViewDadosCSV.Columns.Count; i++)
+            {
+                sw.Write(dataGridViewDadosCSV.Columns[i].HeaderText);
+                if (i != dataGridViewDadosCSV.Columns.Count)
+                {
+                    sw.Write(separator);
+                }
+            }
+            // add new line
+            sw.Write(sw.NewLine);
+            // iterate through all the rows within the gridview
+            for (int i = 0; i < dataGridViewDadosCSV.Rows.Count; i++)
+            {
+                if (filter == filterExport[i])
+                {
+                    // iterate through all colums of specific row
+                    for (int j = 0; j < dataGridViewDadosCSV.Columns.Count; j++)
+                    {
+                        // write particular cell to csv file
+                        sw.Write(dataGridViewDadosCSV[j, i].Value);
+                        if (j != dataGridViewDadosCSV.Columns.Count)
+                        {
+                            sw.Write(separator);
+                        }
+                    }
+                }
+                if (filter == filterExport[i])
+                {
+                    // write new line
+                    sw.Write(sw.NewLine);
+                }
+                
+            }
+            // flush from the buffers.
+            sw.Flush();
+            // closes the file
+            sw.Close();
+        }
+
+        private void messageFinish()
+        {
+            string message = "Archivos generados con successo!";
+            string caption = "Deseas hacer el filtro de otro archivo?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+
+            if (result == DialogResult.Yes)
+            {
+                dataGridViewDadosCSV.DataSource = null;
+                dataGridViewDadosCSV.Rows.Clear();
+                textBoxFileCSV.Clear();
+            }
+            if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void comboBoxColum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filterColumm = comboBoxColum.SelectedIndex;
+        }
+
+        private void comboBoxSeparador_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            separator = comboBoxSeparador.Text;
+        }
+    }
+}
